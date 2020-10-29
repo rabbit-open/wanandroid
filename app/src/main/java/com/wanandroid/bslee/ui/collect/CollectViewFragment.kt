87db0889095584b13e7f68afcf993a296a1b3c43
B.S.Lee <254608684@qq.com>
2020-10-29 16:24:00 +0800

    签名文件和修复bug

--- app/src/main/java/com/wanandroid/bslee/ui/collect/CollectViewFragment.kt
+++ app/src/main/java/com/wanandroid/bslee/ui/collect/CollectViewFragment.kt
@@ -2,11 +2,13 @@ package com.wanandroid.bslee.ui.collect
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wanandroid.bslee.R
 import com.wanandroid.bslee.base.BaseFragment
 import com.wanandroid.bslee.network.WanAndroidApi
+import com.wanandroid.bslee.observer.ResponseObserver
+import com.wanandroid.bslee.vo.ArticleVO
+import com.wanandroid.bslee.vo.PageVO
 import kotlinx.android.synthetic.main.collectfragment.*
 
 class CollectViewFragment : BaseFragment() {
@@ -30,21 +32,29 @@ class CollectViewFragment : BaseFragment() {
     private fun firstLoad() {
         page = 0;
         WanAndroidApi.get().collectPage(page).observe(viewLifecycleOwner,
-            Observer {
-                val newList = if (it.data != null) it.data!!.datas else arrayListOf();
-                collectAdapter.data = newList.toMutableList();
-                srlRefreshLay.finishRefresh();
-                srlRefreshLay.setEnableLoadMore(it.data!!.pageCount > ++page)
+            object : ResponseObserver<PageVO<ArticleVO>>() {
+                override fun onSuccess(t: PageVO<ArticleVO>?) {
+                    t?.apply {
+                        collectAdapter.data = t.datas.toMutableList();
+                        srlRefreshLay.run {
+                            finishRefresh();
+                            setEnableLoadMore(pageCount > ++page)
+                        }
+                    }
+                }
             })
     }
 
     private fun moreLoad() {
         WanAndroidApi.get().collectPage(page).observe(viewLifecycleOwner,
-            Observer {
-                val newList = if (it.data != null) it.data!!.datas else arrayListOf();
-                collectAdapter.data.addAll(newList.toMutableList());
-                srlRefreshLay.setEnableLoadMore(it.data!!.pageCount > ++page)
-                srlRefreshLay.finishLoadMore();
+            object : ResponseObserver<PageVO<ArticleVO>>() {
+                override fun onSuccess(t: PageVO<ArticleVO>?) {
+                    t?.run {
+                        collectAdapter.data.addAll(t.datas.toMutableList());
+                        srlRefreshLay.setEnableLoadMore(t.pageCount > ++page)
+                        srlRefreshLay.finishLoadMore();
+                    }
+                }
             })
     }
 
